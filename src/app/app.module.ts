804b4ef0a0f0d1b139d2009c6049150295bc7a7b
Arthur <arthurfrezende@gmail.com>
2018-11-04 18:58:58 -0200

    Created a provider
    Provider to make GET requests on the OMDb API.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MovieServiceProvider } from '../providers/movie-service/movie-service';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MovieServiceProvider
   ]
 })
 export class AppModule {}
