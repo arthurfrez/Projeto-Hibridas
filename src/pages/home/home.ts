7b62a9635307f0dac13be3a01ddf5a0dfd430ca5
Arthur <arthurfrezende@gmail.com>
2018-11-04 15:12:27 -0200

    Added Tabs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MainPage } from '../main/main';
+import { FavoritesPage } from '../favorites/favorites';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  entryComponents: [ MainPage ]
 })
 export class HomePage {
+
+  mainPage = MainPage;
+  favPage = FavoritesPage;
 
   constructor(public navCtrl: NavController) {
 
