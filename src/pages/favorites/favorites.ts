679c9d0b7d2692843892d7f81afab65b925e8e70
Arthur <arthurfrezende@gmail.com>
2018-11-04 15:59:08 -0200

    Import Fix

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { FavoritesPage } from './favorites';
 
 @Component({
   selector: 'page-favorites',
