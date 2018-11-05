d672276ed66083571bb102f96dd15bd25930bf55
Arthur <arthurfrezende@gmail.com>
2018-11-04 22:17:17 -0200

    Handling No Description

--- src/models/hero.ts
+++ src/models/hero.ts
@@ -1,5 +1,13 @@
 export class Hero {
   name:        string;
   thumb:       string;
-  description: string;
+  private _description: string;
+
+  get description():string {
+    return this._description || "No description.";
+  }
+
+  set description(desc: string) {
+    this._description = desc;
+  }
 }
